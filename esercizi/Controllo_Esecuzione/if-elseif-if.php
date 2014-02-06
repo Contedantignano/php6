eceab3b88c1d4f2e16992613d3a10b70935057c6
gabriele stampa <gabrielestampa@gmail.com>
2014-02-07 00:48:03 +0100

    IF-ELSE-IF PHP EXAMPLE

---
+++ esercizi/Controllo_Esecuzione/if-elseif-if.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+    <title>IF-ELSE-IF PHP</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<h2>IF ELESE IF CONTROLLO DELLA RIPRODUZIONE DEL CODICE</h2>
+<?php include 'include_menu.php'; ?>
+<hr>
+<br>
+<?php
+    $anni = 54;
+    $riduzione = FALSE;
+    $biglietto = 0;
+        if ($anni <= 12)
+            /** l'operatore indica NON Maggiore  */
+        {
+            print ("Hai $anni anni, quindi hai diritto allo sconto<br>");
+        $biglietto = 10;
+        }
+        elseif (($anni > 12)&&$riduzione)
+           {
+            print ("Hai hai diritto ad una riduzione<br>");
+            $biglietto = 15;
+           }
+           else
+           {
+            print ("Non hai diritto a sconti o riduzioni<br>");
+            $biglietto =  30;
+           }
+            print ("Il tuo biglietto costa: $biglietto euro<br>");
+?>
+</body>
+</html>
--- esercizi/Controllo_Esecuzione/index.php
+++ esercizi/Controllo_Esecuzione/index.php
@@ -8,8 +8,8 @@
 <?php include 'include_menu.php'; ?>
 <h3>I tipi di dati in php sono 4</h3>
 <ul>
-    <li><a href="/esercizi/controllo_esecuzione/istruzione_if.php">Istruzione IF</a></li>
-
+    <li><a href="/esercizi/controllo_esecuzione/istruzione_if.php" style="text-decoration:none">Istruzione IF</a></li>
+    <li><a href="/esercizi/controllo_esecuzione/if-elseif-if.php" style="text-decoration:none">Istruzione IF-ELSEIF-ELSE</a></li>
 
 
 </ul>
