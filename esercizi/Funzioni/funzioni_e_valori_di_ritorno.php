f4bb3afda0419b5a0b53ecac1372fbc34562a66d
gabriele stampa <gabrielestampa@gmail.com>
2014-02-09 22:37:58 +0100

    Studio e test su funzioni PHP

--- esercizi/Funzioni/funzioni_e_valori_di_ritorno.php
+++ esercizi/Funzioni/funzioni_e_valori_di_ritorno.php
@@ -1,4 +1,7 @@
 <?php
+/** Nella prima parte del codice vengono dichiarate la funzioni. Così è la prassi.
+ *  Serve a mantenere il codice più ordinato ed è una buona consuetudine.
+ */
     function operazione($a, $op, $b)
     {
       switch($op)
@@ -27,13 +30,17 @@
 <hr>
 <br>
 <?php
+    /** Nella prima parte di codice - dopo la funzione - recupero i dati */
     $valore1 = 10;
     $valore2 = 48;
     print ("valore1 = $valore1<br>");
     print ("valore2 = $valore2<br>");
 
+    /** costruisco un array dove inserisco le operazioni */
+
     $operazioni = array("più","meno","per","diviso","pippo");
 
+/** Utilizzo foreach per CICLARE all'interno dell'Array ($v = è la chiave) */
     foreach ($operazioni as $v)
     {
         print ("$valore1 $v $valore2 = ");
